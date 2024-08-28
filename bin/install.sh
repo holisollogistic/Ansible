f0493d1099c94474991ec2abc7aa8dbae30d6ac2
kapil <kapil.kumar@holisollogistics.com>
2024-08-28 10:03:21 +0530

    tdestdd1

--- bin/install.sh
+++ bin/install.sh
@@ -15,7 +15,7 @@ sudo chmod +x /usr/local/bin/vh
 echo "################### cloning virtualhost with MultiPHP repo ###########################"
 
 rm -rf ~/vh
-git clone https://github.com/naresh335/ansible-virutalhost-multiPHP.git ~/vh
+git clone https://github.com/holisollogistic/Ansible.git ~/vh
 echo "\n\ncopy & modify this command or run '$(tput bold)vh -h$(tput sgr0)' for getting this command during any session\n\n"
 echo 'Usage: vh -php 8.1 -url app.test'
 exec ${SHELL}
